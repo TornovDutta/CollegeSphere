1032a0319b965ac9180b3178559dd557afbb2092
TornovDutta <tornovdutta@gmail.com>
2025-10-27 19:04:23 +0530

    first commit

---
+++ src/main/java/org/example/collegesphere/CollegeSphereApplication.java
@@ -0,0 +1,13 @@
+package org.example.collegesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeSphereApplication.class, args);
+    }
+
+}
