8e2d7b5188a3ccd3b57f5ec74243d1012335d5ee
TornovDutta <tornovdutta@gmail.com>
2025-10-29 17:31:00 +0530

    add the get all

--- src/main/java/org/example/collegesphere/service/UserSerivce.java
+++ src/main/java/org/example/collegesphere/service/UserSerivce.java
@@ -1,13 +1,22 @@
 package org.example.collegesphere.service;
 
 import org.example.collegesphere.model.Users;
+import org.example.collegesphere.repo.UserRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserSerivce {
-    public List<Users> get() {
+    private final UserRepo userRepo;
 
+    public UserSerivce(UserRepo userRepo) {
+        this.userRepo = userRepo;
     }
+
+    public List<Users> get() {
+        return userRepo.findAll();
+    }
+
+
 }
