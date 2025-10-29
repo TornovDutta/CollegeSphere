26557f9a13c3042a286a96f01d14c77e285ee5ea
TornovDutta <tornovdutta@gmail.com>
2025-10-29 17:29:31 +0530

    add repo

---
+++ src/main/java/org/example/collegesphere/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package org.example.collegesphere.repo;
+
+import org.example.collegesphere.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+}
