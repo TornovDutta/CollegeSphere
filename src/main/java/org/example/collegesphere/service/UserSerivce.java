da85ad02b019ca06ae9276fdd816036160a2bde1
TornovDutta <tornovdutta@gmail.com>
2025-10-29 17:32:51 +0530

    add the get by user id

--- src/main/java/org/example/collegesphere/service/UserSerivce.java
+++ src/main/java/org/example/collegesphere/service/UserSerivce.java
@@ -4,7 +4,9 @@ import org.example.collegesphere.model.Users;
 import org.example.collegesphere.repo.UserRepo;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserSerivce {
@@ -19,4 +21,11 @@ public class UserSerivce {
     }
 
 
+    public Users getID(int id) {
+        Optional<Users> users=userRepo.findById(id);
+        if(users.isPresent()){
+            return users.get();
+        }
+        return null;
+    }
 }
